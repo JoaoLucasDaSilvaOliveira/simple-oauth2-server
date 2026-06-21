7ba058a002170fd9652f963a1eee6bb746c1dcbc
João Lucas <jlso9866@gmail.com>
2026-06-21 12:36:56 -0300

    chore: removed other service add by mistake

--- login-service/src/routes/login.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { login } = require('../controllers/login.controller');
-
-const router = express.Router();
-
-// Single responsibility: expose the login endpoint.
-router.post('/login', login);
-
-module.exports = router;
